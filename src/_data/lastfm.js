be4d89e41c6b8f9ea2fd05749a2179faaa5e5e66
fLaMEd <shadybrothers@gmail.com>
2021-04-10 12:58:17 +1200

    adjust lastfm.js

--- src/_data/lastfm.js
+++ src/_data/lastfm.js
@@ -8,7 +8,7 @@ const API = 'http://ws.audioscrobbler.com/2.0/';
 module.exports = async () => {
   try {
     const json = await Cache(
-      `${API}?method=user.getrecenttracks&user=${USERNAME}&limit=12&api_key=${API_KEY}&format=json`,
+      `${API}?method=user.getrecenttracks&user=${USERNAME}&limit=11&api_key=${API_KEY}&format=json`,
       {
         duration: '2h',
         type: 'json',
